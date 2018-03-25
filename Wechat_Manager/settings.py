eb6e0fbe5c19246b8b033562773d239274469129
iamnwi <iamnwi@gmail.com>
2018-03-26 01:40:53 +0800

    added allowd host, no cors

--- Wechat_Manager/settings.py
+++ Wechat_Manager/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'Wechat_Manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'OPTIONS': {
+            'read_default_file': '/home/www/Wechat_Manager/Wechat_Manager/mysql.cnf',
+        },
     }
 }
 
