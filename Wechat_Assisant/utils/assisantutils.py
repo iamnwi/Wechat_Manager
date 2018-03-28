402662f6e80b4ce76add192631eccbfe07263a58
iamnwi <iamnwi@gmail.com>
2018-03-28 12:24:35 +0800

    added group msg handler

--- Wechat_Assisant/utils/assisantutils.py
+++ Wechat_Assisant/utils/assisantutils.py
@@ -10,6 +10,102 @@ from Wechat_Assisant.models import *
 
 sendFilePrefixDict = {'Attachment': '@fil@', 'Picture': '@img@', 'Video': '@vid@'}
 
+def get_group_notify_context(notify_msg):
+	returnList = []
+	to_user_name = notify_msg.to_user_name
+	group_name = notify_msg.group_name
+	msg_time = notify_msg.msg_time
+	start = msg_time - 60
+	end = msg_time + 60
+	group_msg_qs = Message.objects.filter(msg_is_group=True, group_name=group_name, msg_time__range=(start, end))
+	if group_msg_qs.count() > 0:
+		group_nick_name = get_group_nick_name(notify_msg.group_name)
+		send_text = u'您在群聊 "'+ group_nick_name + u'" 中收到和您有关的消息：\n'
+		# concatenate messages, converting recording to text
+		print("group msg qs:")
+		print(group_msg_qs)
+		for msg in group_msg_qs.iterator():
+			if msg.msg_type == 'Recording':
+				content = '[Recording]'
+			elif msg.msg_type == 'Picture':
+				content = '[Picture]'
+			elif msg.msg_type == 'Text':
+				content = msg.msg_text
+			send_text += msg.sender_nick_name + u': ' + content + u'\n'
+		send_text += u'---------------------------'
+		returnList.append(send_text)
+	return returnList
+
+# send all groups' notify messages to filehelper
+def assisant_send_group_notify(user_name):
+	print("Send group notify messages to filehelper")
+	notify_msg_qs = NotifyMessage.objects.filter(to_user_name=user_name)
+	if notify_msg_qs.count()==0:
+		itchat.send(u'没有提及你的群消息', toUserName='filehelper')
+		return
+	else:
+		msg_list = []
+		for notify_msg in notify_msg_qs.iterator():
+			msg_list += get_group_notify_context(notify_msg)
+			notify_msg.delete()
+		for text in msg_list:
+			itchat.send(text, toUserName='filehelper')
+	return
+
+# trigger different assisant functions according to the msg content
+def assisant_control_menue(msg):
+	msg_from = msg['FromUserName']
+	print('robot control')
+	if msg['Type'] == 'Text':
+		msgContent = msg['Text']
+		if msgContent == '@':
+			assisant_send_group_notify(msg_from)
+		else:
+			print('It is an unsupported control message.')
+	else:
+		print('It is an unsupported type of control message.')
+
+# find display name to the specific user in the group where the message comes from.
+def get_display_name_group(msg, user_name):
+	member_content = re.search(r"<ContactList: \[<ChatroomMember: \{(.*)\}>]>", str(msg))
+	if member_content != None:
+		print(member_content.group(1))
+		users = re.findall(r"'UserName': '([^']*)", member_content.group(1))
+		displays = re.findall(r"'DisplayName': '([^']*)", member_content.group(1))
+		if users != None and displays != None:
+			user2nick = dict(zip(users, displays))
+			for (user, nick) in user2nick.items():
+				if user == user_name:
+					return nick
+
+# check whether it is a notify message
+# 1. check @all
+# 2. check @NickName
+# 3. check @DisplayName(a name defined for a group)
+def check_group_notify(msg, group_name):
+	if not msg['Type']=='Text':
+		return False
+	print('checkGroupNotify')
+	msg_content = msg['Content']
+	if '@' in msg_content:
+		if u'@所有人' in msg_content or\
+			u'@all' in msg_content:
+			return True
+		else:
+			user_name = msg['ToUserName']
+			nick_name = get_nick_name(user_name)
+			if u'@'+nick_name in msg_content:
+				return True
+			display_name = get_display_name_group(msg, user_name)
+			print("your display name is %s" % display_name)
+			if display_name != None and '@' + display_name in msg_content:
+				return True
+	else:
+		return False
+
+# call back fuction
+# provoked when a user log out
+# turn his/her on-line status to off-line
 def turn_offline():
 	uin = (itchat.search_friends())['Uin']
 	wc = get_wc(uin=uin)
@@ -71,25 +167,23 @@ def note_handler(msg):
 				os.rename(newPath, tmp.name)
 				print(r)
 
-
-
 #将接收到的消息存放在字典中，当接收到新消息时对字典中超时的消息进行清理
 #没有注册note（通知类）消息，通知类消息一般为：红包 转账 消息撤回提醒等，不具有撤回功能
 @itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS, NOTE])
 def msg_handler(msg):
-	# ignore msg sent by a user here in client pool and receive it again
-	# detail:	wechat will send msg to both sender and receiver
-	# 			the msg sender got contain a from_user_name that my server know
-	# 			but a to_user_name that my server doesn't know
-	# 			user this feature to filter the msg turned back to the sender
-	if WechatClient.objects.filter(user_name=msg['ToUserName']).count() == 0:
-		return
 	# ignore the msg which a user send to himself/herself
 	if msg['FromUserName'] == msg['ToUserName']:
 		return;
 	# it maybe is a robot control message
 	if msg['ToUserName'] == 'filehelper':
-		robotControl(msg)
+		assisant_control_menue(msg)
+		return
+	# ignore msg sent by a user here in the client pool and receive it again
+	# detail:	wechat will send msg to both sender and receiver
+	# 			the msg sender got a msg containing a from_user_name that my server know
+	# 			but a to_user_name that my server doesn't know
+	# 			user this feature to filter the msg turned back to the sender
+	if WechatClient.objects.filter(user_name=msg['ToUserName']).count() == 0:
 		return
 
 	print('%s received a msg' % get_nick_name(msg['ToUserName']))
@@ -101,3 +195,36 @@ def msg_handler(msg):
 	# insert this msg to DB
 	msg_obj = Message.create(msg);
 	msg_obj.save()
+
+# group texts handling function
+# Issues: group msg will not boardcast to the sender, so we miss part of the notify context
+# TO-DO: 1. Can we tackle the boardcast redundancy? boardcast msg will be sent to every user in a group
+#			if we have multiple clients in a same group, then a same msg will repeated in our DB
+# 		2. How can we identical different groups? now a group will create a new model after users login again
+@itchat.msg_register([TEXT, RECORDING, PICTURE], isGroupChat=True)
+def HandleGroupMsg(msg):
+	print('%s received a group msg' % get_nick_name(msg['ToUserName']))
+
+	# initial a group or update nick name of a gorup
+	group_name = msg['FromUserName']
+	group_nick_name = msg['User']['NickName']
+	group_qs = Group.objects.filter(name=msg['FromUserName'])
+	if group_qs.count() == 0:
+		group_obj = Group(name=group_name, nick_name=group_nick_name)
+		group_obj.save()
+	elif group_qs.count() == 1 and group_nick_name != group_qs.get(name=group_name).nick_name:
+		group_obj = group_qs.get(name=group_name)
+		group_obj.nick_name = group_nick_name
+		group_obj.save()
+
+	# create messages model
+	msg_obj = Message.create(msg, is_group=True);
+	msg_obj.save()
+
+	# check whether it is a notify message
+	if check_group_notify(msg, group_name):
+		to_user_name = msg['ToUserName']
+		msg_time = msg['CreateTime']
+		notify_msg = NotifyMessage(to_user_name=to_user_name, group_name=group_name, msg_time=msg_time)
+		notify_msg.save()
+		print('%s @%s in a group' % (msg['ActualNickName'], get_display_name_group(msg, to_user_name)))
