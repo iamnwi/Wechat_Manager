b70ff56e67c9db8975677de5aa69d9c106d2bd6c
iamnwi <iamnwi@gmail.com>
2018-04-06 13:29:52 +0800

    integrated werobot

--- Wechat_Assisant/urls.py
+++ Wechat_Assisant/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     url(r'^(?i)index', views.index, name='index'),
     url(r'^(?i)pushlogin', views.pushlogin, name='pushlogin'),
-    # url(r'^wxmp/', make_view(mp_robot), name='mp_robot'),
-    url(r'^(?i)wxmp', views.wxmp, name='wxmp'),
+    url(r'^(?i)wxmp', make_view(mp_robot), name='mp_robot'),
+    #url(r'^(?i)wxmp', views.wxmp, name='wxmp'),
 ]
--- Wechat_Assisant/utils/mp_handler.py
+++ Wechat_Assisant/utils/mp_handler.py
@@ -12,6 +12,8 @@ def run_mp():
     # p.start()
     # access_token = get_access_token(settings.MP_APP_ID)
     mp_robot = WeRoBot(token=settings.MP_TOKEN)
+    mp_robot.config["APP_ID"] = "wx8a9b7b3c741a2414"
+    mp_robot.config["APP_SECRET"] = "07004aa665d414d3ad0b1abb34ff43b7"
 
     @mp_robot.handler
     def hello(message):
@@ -20,3 +22,11 @@ def run_mp():
     return mp_robot
 
 mp_robot = run_mp()
+#mp_robot = WeRoBot(token='520william')
+#mp_robot.config["APP_ID"] = "wx8a9b7b3c741a2414"
+#mp_robot.config["APP_SECRET"] = "07004aa665d414d3ad0b1abb34ff43b7"
+
+
+#@mp_robot.handler
+#def hello(message):
+#    return 'Hello World!'
