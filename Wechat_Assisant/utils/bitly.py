d4c179067a595777af0f5e67d886430bee3dd20a
iamnwi <iamnwi@gmail.com>
2018-04-07 16:06:06 +0800

    added bitly.py

---
+++ Wechat_Assisant/utils/bitly.py
@@ -0,0 +1,16 @@
+import requests
+from django.conf import settings
+
+def shorten(uri):
+    query_params = {
+        'access_token': setting.BITLY_ACCESS_TOKEN,
+        'longUrl': uri
+    }
+    endpoint = 'https://api-ssl.bitly.com/v3/shorten'
+    response = requests.get(endpoint, params=query_params, verify=False)
+
+    data = response.json()
+    if not data['status_code'] == 200:
+        logger.error("Unexpected status_code: {} in bitly response. {}".format(data['status_code'], response.text))
+
+    return data['data']['url']
