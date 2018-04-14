2266fc8d9305f0c28e34f572b05cdb423b68b299
iamnwi <iamnwi@gmail.com>
2018-04-14 11:31:06 +0800

    strengthened MP UX, added MP interactive msgs

--- Wechat_Assisant/utils/mp_handler.py
+++ Wechat_Assisant/utils/mp_handler.py
@@ -19,26 +19,26 @@ def run_mp():
     mp_robot = WeRoBot(token=settings.MP_TOKEN)
     mp_robot.config["APP_ID"] = settings.MP_APP_ID
     mp_robot.config["APP_SECRET"] = settings.MP_APP_SECRET
-    greet = "Hi, nice to meet you! Why don't you input 'login' to enjoy our service? :)"
-    help_menue = \
-    """Oh, I bet you feel confused now. Actually, only one thing you need to do currently:
-Input 'login'"""
 
     @mp_robot.subscribe
     def subscribe(message):
-        return 'Hello My Friend!'
+        return settings.MP_GREET
 
     @mp_robot.text
     def text_reply(message):
         if re.match('login', message.content, re.IGNORECASE):
             push_res = mp_pushlogin(message)
             if push_res:
-                rely_text = 'please wait and comfirm login on you phone'
+                rely_text = settings.MP_COMFIRM_LOGIN
                 return rely_text
             else:
                 return mp_login(message)
+        elif re.match('function', message.content, re.IGNORECASE):
+            return settings.MP_FUNCTION
+        elif re.match('how', message.content, re.IGNORECASE):
+            return settings.MP_HOW_LOGIN
         else:
-            return help_menue
+            return settings.MP_HELP
 
     def mp_login(message):
         from_openid = message.source
@@ -53,7 +53,7 @@ Input 'login'"""
             obj = ShortUrl(openid=from_openid, login_url=s_url)
             obj.save()
         # create reply msg
-        rely_text = 'please use another device to browse the webpage %s to finish login' % s_url
+        rely_text = "%s\n%s" % (settings.MP_LOGIN_VIA_LINK, s_url)
         return rely_text
 
     def mp_pushlogin(message):
